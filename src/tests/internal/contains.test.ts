83dacc1c2a6e601e78647d89f90b1dd09739a976
Yoeri Nijs <yoeri@posteo.net>
2020-10-20 20:17:46 +0200

    Updated test scenario

--- src/tests/internal/contains.test.ts
+++ src/tests/internal/contains.test.ts
@@ -32,17 +32,17 @@ test('It should return false when the value is not in a nested array', () => {
 
 it('It should return true if one of the expecting values is in the array', () => {
     const arr = ['do', 're', 'mi'];
-    const result = contains<string>(arr, 'fa', 'do');
+    const result = contains<string>(arr, 'fa', 'so', 'la', 'ti', 'do');
     expect(result).toBe(true);
 });
 
 it('It should return true if one of the expecting values is in a nested array', () => {
     const arr = [['do'], 're', 'mi'];
-    const result = contains<string>(arr, 'fa', 'do');
+    const result = contains<string>(arr, 'fa', 'so', 'la', 'ti', 'do');
     expect(result).toBe(true);
 });
 
-it('It should return true if all values are not in the array', () => {
+it('It should return false if all values are not in the array', () => {
     const arr = ['do', 're', 'mi'];
     const result = contains<string>(arr, 'fa', 'so');
     expect(result).toBe(false);
